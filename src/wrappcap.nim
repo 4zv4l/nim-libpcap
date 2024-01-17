5e59ddd9c3a32540f4b0dc7ce919737b60e5ef34
azz <azz@lexibct.com>
2024-01-17 16:40:50 +0800

    boom

---
+++ src/wrappcap.nim
@@ -0,0 +1,36 @@
+## Nim basic wrapper wround libpcap
+
+import std/[strformat]
+import ./wrappcap/libpcap
+
+type
+    LibPcapError = object of Exception
+
+var errbuf: array[1024, char]
+proc err*(): cstring {.inline.} =
+    ## libpcap error handling
+    ##
+    ## Example:
+    ## ```
+    ## let nOfDevs = pcapFindAllDevs(devs, err())
+    ## if nOfDevs == 0:
+    ##     echo "error: ", $error()
+    ## ```
+    cast[cstring](errbuf[0].addr)
+
+proc findAllDevs*(): seq[string] =
+    ## Return a seq of interfaces as string
+    var devs: PcapIf
+    let nOfDevs = pcapFindAllDevs(devs, err())
+    if nOfDevs != 0:
+        raise newException(LibPcapError, fmt"Error with pcapFindAllDevs: {err()}")
+    var dev = devs # allow to save devs to free memory later
+    while dev.next != nil:
+        result.add($dev.name)
+        dev = dev.next
+    # TODO: fix this not working (valgrind says no leak on my side)
+    #pcapFreeAllDevs(devs)
+
+# TODO: learn how to raise exception :3
+if pcapInit(0,err()) != 0:
+    raise newException(LibPcapError, fmt"Error when running pcapInit: {err()}")
---
+++ wrappcap.nimble
@@ -0,0 +1,11 @@
+# Package
+
+version       = "0.1.0"
+author        = "azz"
+description   = "A wrapper for libpcap"
+license       = "MIT"
+srcDir        = "src"
+
+
+# Dependencies
+requires "nim >= 1.6.3"
